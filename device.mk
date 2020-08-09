ded208a89ab167b8cf0450bcd4fc041ee770cd43
Jake Weinstein <jake@aospa.co>
2020-08-09 14:32:32 +0000

    sapphire: Define TARGET_BOARD_PLATFORM in device makefile
    This allows us to use it in QC Split Product Makefiles
    
    Change-Id: Ibd7493eb8c266b524a8f51aad658837798cc3d2d
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- device.mk
+++ device.mk
@@ -451,6 +451,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
+# Platform
+TARGET_BOARD_PLATFORM := bengal
+TARGET_BOARD_SUFFIX := _515
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
