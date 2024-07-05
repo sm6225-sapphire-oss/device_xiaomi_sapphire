5c15a0c3e903689ce018fec4534a6d61b8db8704
hefatss <ashleyy99@proton.me>
2024-07-05 07:10:09 +0000

    sapphire: Drop Dolby Atmos
    Change-Id: If1580bd793f459a0ade889fa1f6bb15af8773c98

--- device.mk
+++ device.mk
@@ -66,9 +66,6 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     libvolumelistener
 
-PRODUCT_PACKAGES += \
-    XiaomiDolby
-
 AUDIO_HAL_DIR := hardware/qcom-caf/sm6225/audio/primary-hal
 
 PRODUCT_COPY_FILES += \
@@ -343,11 +340,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
-    libavservices_minijail_vendor \
     libcodec2_hidl@1.0.vendor \
-    libcodec2_hidl@1.1.vendor \
-    libcodec2_hidl@1.2.vendor \
-    libcodec2_soft_common.vendor \
     libcodec2_vndk.vendor \
     libsfplugin_ccodec_utils.vendor \
     libplatformconfig
--- extract-files.sh
+++ extract-files.sh
@@ -58,15 +58,11 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
-        vendor/lib*/soundfx/libdlbvol.so|vendor/lib*/soundfx/libhwdap.so|vendor/lib64/libdlbdsservice.so | vendor/lib64/libcodec2_soft_ac4dec.so | vendor/lib64/libcodec2_soft_ddpdec.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
-            ;;    
         vendor/lib64/hw/displayfeature.default.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
-        vendor/bin/hw/vendor.qti.media.c2@1.0-service|vendor/bin/hw/vendor.dolby.media.c2@1.0-service|vendor/bin/hw/vendor.qti.media.c2audio@1.0-service)
+        vendor/bin/hw/vendor.qti.media.c2@1.0-service|vendor/bin/hw/vendor.qti.media.c2audio@1.0-service)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libcodec2_hidl@1.0.so" "${2}"
             "${PATCHELF}" --add-needed "libshim.so" "${2}"
@@ -87,6 +83,7 @@ function blob_fixup() {
         *)
             return 1
             ;;
+
     esac
 
     return 0
