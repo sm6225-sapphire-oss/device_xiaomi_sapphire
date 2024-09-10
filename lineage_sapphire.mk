dcab7333c93351372da856db5cde4a0f99ebb5df
kibria5 <mdkibria687@gmail.com>
2024-09-10 19:16:13 +0600

    sapphire: Crdroid Bringup

--- lineage_sapphire.mk
+++ lineage_sapphire.mk
@@ -18,6 +18,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES = 1080
 TARGET_HAS_UDFPS := true
 
+# Inherit some common crDroid stuff
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+
 PRODUCT_NAME := lineage_sapphire
 PRODUCT_DEVICE := sapphire
 PRODUCT_MANUFACTURER := Xiaomi
